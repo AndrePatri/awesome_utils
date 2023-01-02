1449287c25563c0074f9715fbd93ceb3535b734f
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2023-01-02 23:14:07 +0100

    reenabled awesome leg contact estimation

--- tests/test_contact_est.cpp
+++ tests/test_contact_est.cpp
@@ -55,120 +55,116 @@ class TestContactEst: public ::testing::Test {
 
 };
 
-//TEST_F(TestContactEst, compute_quantities_fixed_base)
-//{
-//    Eigen::VectorXd q, v, a, tau, p,
-//                    tau_c;
+TEST_F(TestContactEst, compute_quantities_fixed_base)
+{
+    Eigen::VectorXd q, v, a, tau, p,
+                    tau_c;
 
-//    utils_defs::Force3D f_c;
-//    utils_defs::Torque3D t_c;
-//    utils_defs::SpatialJac J;
+    utils_defs::Force3D f_c;
+    utils_defs::Torque3D t_c;
+    utils_defs::SpatialJac J;
 
-//    Eigen::MatrixXd B, C;
+    Eigen::MatrixXd B, C;
 
-//    Model::Ptr model_ptr(new Model(urdf_path_fixed));
+    Model::Ptr model_ptr(new Model(urdf_path_fixed));
 
-//    model_ptr->get_state(q, v, a, tau);
-//    Eigen::VectorXd base_position = Eigen::VectorXd::Zero(3);
-//    Eigen::VectorXd base_orient = Eigen::VectorXd::Zero(4);
-//    q << 0.0, 0.0, 0.0; // leg vertical with horizontal base link
-//    v << 0.0, 0.0, 0.0; // static
-//    a << 0.0, 0.0, 0.0; // static
-//    tau << 0.0, 0.0, 0.0;
-//    model_ptr->set_q(q);
-//    model_ptr->set_v(v);
-//    model_ptr->set_a(a);
-//    model_ptr->set_tau(tau);
+    model_ptr->get_state(q, v, a, tau);
+    Eigen::VectorXd base_position = Eigen::VectorXd::Zero(3);
+    Eigen::VectorXd base_orient = Eigen::VectorXd::Zero(4);
+    q << 0.0, 0.0, 0.0; // leg vertical with horizontal base link
+    v << 0.0, 0.0, 0.0; // static
+    model_ptr->set_q(q);
+    model_ptr->set_v(v);
 
-//    model_ptr->update(); // computes all terms
+    model_ptr->update(); // computes all terms
 
-//    double dt = 0.01; // control loop dt
-//    double BW = 200.0; // observer bandwitdth [Hz]
+    double dt = 0.0001; // control loop dt
+    double BW = 10000.0; // observer bandwitdth [Hz]
 
-//    MomentumBasedFObs::Reg6D lambda = Eigen::VectorXd::Zero(6);
-//    lambda << 0.001, 0.001, 0.001, 0.01, 0.01, 0.01;
+    MomentumBasedFObs::Reg6D lambda = Eigen::VectorXd::Zero(6);
+    lambda << 0.00001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001;
 
-//    std::vector<int> selector{0, 1, 2}; // only force
+    std::vector<int> selector{0, 1, 2}; // only force
 
-//    std::vector<std::string> contacts{"tip1"};
+    std::vector<std::string> contacts{"tip1"};
 
-//    bool regularize_w = true;
+    bool regularize_w = true;
 
-//    MomentumBasedFObs::Ptr f_obs_ptr(new MomentumBasedFObs(model_ptr, dt,
-//                                                           contacts,
-//                                                           BW,
-//                                                           lambda, regularize_w,
-//                                                           selector));
+    MomentumBasedFObs::Ptr f_obs_ptr(new MomentumBasedFObs(model_ptr, dt,
+                                                           contacts,
+                                                           BW,
+                                                           lambda, regularize_w,
+                                                           selector));
 
-//    f_obs_ptr->update(); // compute estimates using the current state in model_ptr
+    f_obs_ptr->update(); // compute estimates using the current state in model_ptr
 
-//    f_obs_ptr->get_tau_obs(tau_c);
-//    f_obs_ptr->get_f_est_at(contacts[0], f_c);
-//    f_obs_ptr->get_t_est_at(contacts[0], t_c);
+    f_obs_ptr->get_tau_obs(tau_c);
+    f_obs_ptr->get_f_est_at(contacts[0], f_c);
+    f_obs_ptr->get_t_est_at(contacts[0], t_c);
 
-//    std::cout << "** FIXED BASE DEBUG PRINTS**\n"  << std::endl;
-//    std::cout << "\nURDF loaded at: "<< model_ptr->get_urdf_path() << "\n " << std::endl;
-//    std::cout << "** tau_c: \n" << tau_c << "\n " << std::endl;
-//    std::cout << "** f_c: \n" << f_c << "\n " << std::endl;
-//    std::cout << "** w_c: \n" << t_c << "\n " << std::endl;
+    std::cout << "** FIXED BASE DEBUG PRINTS**\n"  << std::endl;
+    std::cout << "\nURDF loaded at: "<< model_ptr->get_urdf_path() << "\n " << std::endl;
+    std::cout << "** tau_c: \n" << tau_c << "\n " << std::endl;
+    std::cout << "** f_c: \n" << f_c << "\n " << std::endl;
+    std::cout << "** w_c: \n" << t_c << "\n " << std::endl;
 
-//}
+}
 
-//TEST_F(TestContactEst, compute_quantities_floating_base)
-//{
-//    Eigen::VectorXd q, v, a, tau, p,
-//                    tau_c, g, CT_v,
-//                    tau_c_raw_static;
+TEST_F(TestContactEst, compute_quantities_floating_base)
+{
+    Eigen::VectorXd q, v, a, tau, p,
+                    tau_c, g, CT_v,
+                    tau_c_raw_static;
 
-//    utils_defs::Force3D f_c;
-//    utils_defs::Torque3D t_c;
-//    utils_defs::SpatialJac J;
+    utils_defs::Force3D f_c;
+    utils_defs::Torque3D t_c;
+    utils_defs::SpatialJac J;
 
-//    Eigen::MatrixXd B, C;
+    Eigen::MatrixXd B, C;
 
-//    Model::Ptr model_ptr(new Model(urdf_path_floating));
+    Model::Ptr model_ptr(new Model(urdf_path_floating));
 
-//    model_ptr->set_neutral();
-//    model_ptr->get_state(q, v, a, tau);
+    model_ptr->set_neutral();
+    model_ptr->get_state(q, v, a, tau);
 
-//    model_ptr->update(); // computes all terms
+    model_ptr->update(); // computes all terms
 
-//    model_ptr->get_g(g);
-//    model_ptr->get_tau(tau);
-//    tau_c_raw_static = g - tau; // raw disturbance torques (not filtered
+    model_ptr->get_g(g);
+    model_ptr->get_tau(tau);
+    tau_c_raw_static = g - tau; // raw disturbance torques (not filtered
 
-//    double dt = 0.01; // control loop dt
-//    double BW = 200.0; // observer bandwitdth [Hz]
+    double dt = 0.0001; // control loop dt
+    double BW = 10000.0; // observer bandwitdth [Hz]
 
-//    MomentumBasedFObs::Reg6D lambda = Eigen::VectorXd::Zero(6);
-//    lambda << 0.001, 0.001, 0.001, 0.01, 0.01, 0.01;
+    MomentumBasedFObs::Reg6D lambda = Eigen::VectorXd::Zero(6);
+    lambda << 0.00001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001;
 
-//    std::vector<int> selector{0, 1, 2}; // only force
+    std::vector<int> selector{0, 1, 2}; // only force
 
-//    std::vector<std::string> contacts{"tip1"};
+    std::vector<std::string> contacts{"tip1"};
 
-//    bool regularize_w = true;
+    bool regularize_w = true;
 
-//    MomentumBasedFObs::Ptr f_obs_ptr(new MomentumBasedFObs(model_ptr, dt,
-//                                                           contacts,
-//                                                           BW,
-//                                                           lambda, regularize_w,
-//                                                           selector));
+    MomentumBasedFObs::Ptr f_obs_ptr(new MomentumBasedFObs(model_ptr, dt,
+                                                           contacts,
+                                                           BW,
+                                                           lambda, regularize_w,
+                                                           selector));
 
-//    f_obs_ptr->update(); // compute estimates using the current state in model_ptr
+    f_obs_ptr->update(); // compute estimates using the current state in model_ptr
 
-//    f_obs_ptr->get_tau_obs(tau_c);
-//    f_obs_ptr->get_f_est_at(contacts[0], f_c);
-//    f_obs_ptr->get_t_est_at(contacts[0], t_c);
+    f_obs_ptr->get_tau_obs(tau_c);
+    f_obs_ptr->get_f_est_at(contacts[0], f_c);
+    f_obs_ptr->get_t_est_at(contacts[0], t_c);
 
-//    std::cout << "** FLOATING BASE DEBUG PRINTS**\n"  << std::endl;
-//    std::cout << "\nURDF loaded at: "<< model_ptr->get_urdf_path() << "\n " << std::endl;
-//    std::cout << "** tau_c: \n" << tau_c << "\n " << std::endl;
-//    std::cout << "** tau_c_raw: \n" << tau_c_raw_static << "\n " << std::endl;
-//    std::cout << "** f_c: \n" << f_c << "\n " << std::endl;
-//    std::cout << "** w_c: \n" << t_c << "\n " << std::endl;
+    std::cout << "** FLOATING BASE DEBUG PRINTS**\n"  << std::endl;
+    std::cout << "\nURDF loaded at: "<< model_ptr->get_urdf_path() << "\n " << std::endl;
+    std::cout << "** tau_c: \n" << tau_c << "\n " << std::endl;
+    std::cout << "** tau_c_raw: \n" << tau_c_raw_static << "\n " << std::endl;
+    std::cout << "** f_c: \n" << f_c << "\n " << std::endl;
+    std::cout << "** w_c: \n" << t_c << "\n " << std::endl;
 
-//}
+}
 
 TEST_F(TestContactEst, test_contact_est_anymal)
 {
