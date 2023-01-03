d2e168b0a159a1b79d77aff0e064b4f46dabbec8
andrea patrizi <andrepatrizi1b6e6@gmail.com>
2023-01-03 23:18:16 +0100

    added quadruped test

--- tests/test_contact_est.cpp
+++ tests/test_contact_est.cpp
@@ -16,6 +16,8 @@ using namespace ContactEstUtils;
 std::string urdf_path_fixed;
 std::string urdf_path_floating;
 std::string urdf_anymal;
+std::string urdf_quadruped;
+
 IOFormat CleanFmt(4, 0, ", ", "\n", "[", "]");
 
 namespace
@@ -211,7 +213,12 @@ TEST_F(TestContactEst, test_contact_est_anymal)
     Tau_c_raw = Eigen::MatrixXd::Zero(nv, rollout_number);
 
     MomentumBasedFObs::Reg6D lambda = Eigen::VectorXd::Zero(6);
-    lambda << 0.00001, 0.00001, 0.00001, 0.00001, 0.00001, 0.00001;
+    lambda << 0.000000000000001,
+              0.000000000000001,
+              0.000000000000001,
+              0.000000000000001,
+              0.000000000000001,
+              0.000000000000001;
 
     std::vector<int> selector{0, 1, 2}; // only force
 
@@ -256,7 +263,117 @@ TEST_F(TestContactEst, test_contact_est_anymal)
 
     }
 
-    std::cout << "** FLOATING BASE DEBUG PRINTS**\n"  << std::endl;
+    std::cout << "** ANYMAL DEBUG PRINTS**\n"  << std::endl;
+    std::cout << "\nURDF loaded at: "<< model_ptr->get_urdf_path() << "\n " << std::endl;
+    std::cout << "** tau_c: \n" << Tau_c << "\n " << std::endl;
+    std::cout << "** tau_c_raw: \n" << Tau_c_raw << "\n " << std::endl;
+    std::cout << "** Fc_lf: \n" << Fc_lf << "\n " << std::endl;
+    std::cout << "** Fc_lh: \n" << Fc_lh << "\n " << std::endl;
+    std::cout << "** Fc_rf: \n" << Fc_rf << "\n " << std::endl;
+    std::cout << "** Fc_rh: \n" << Fc_rh << "\n " << std::endl;
+    std::cout << "** Tc_lf: \n" << Tc_lf << "\n " << std::endl;
+    std::cout << "** Tc_lh: \n" << Tc_lh << "\n " << std::endl;
+    std::cout << "** Tc_rf: \n" << Tc_rf << "\n " << std::endl;
+    std::cout << "** Tc_rh: \n" << Tc_rh << "\n " << std::endl;
+
+}
+
+TEST_F(TestContactEst, test_contact_est_quadruped)
+{
+    Eigen::VectorXd q, v, a, tau, p,
+                    tau_c, g, CT_v,
+                    tau_c_raw_static;
+
+    utils_defs::Force3D fc_lf, fc_lh, fc_rf, fc_rh;
+    utils_defs::Torque3D tc_lf, tc_lh, tc_rf, tc_rh;
+
+    Model::Ptr model_ptr(new Model(urdf_quadruped));
+
+    model_ptr->set_neutral();
+    model_ptr->get_state(q, v, a, tau);
+
+    model_ptr->update(); // computes all terms
+
+    model_ptr->get_g(g);
+    model_ptr->get_tau(tau);
+    tau_c_raw_static = g - tau; // raw disturbance torques (not filtered
+
+    int nv = model_ptr->get_nv();
+
+    double dt = 0.0001; // control loop dt
+    double BW = 10000.0; // observer bandwitdth [Hz]
+
+    int rollout_number = 15; // number of samples for testing steady state convergence
+    // of the observer over multiple sample intervals
+
+    Eigen::MatrixXd Fc_lf, Fc_lh, Fc_rf, Fc_rh;
+    Eigen::MatrixXd Tc_lf, Tc_lh, Tc_rf, Tc_rh;
+
+    Eigen::MatrixXd Tau_c, Tau_c_raw;
+
+    Fc_lf = Eigen::MatrixXd::Zero(fc_lf.size(), rollout_number);
+    Fc_lh = Eigen::MatrixXd::Zero(fc_lf.size(), rollout_number);
+    Fc_rf = Eigen::MatrixXd::Zero(fc_lf.size(), rollout_number);
+    Fc_rh = Eigen::MatrixXd::Zero(fc_lf.size(), rollout_number);
+    Tc_lf = Eigen::MatrixXd::Zero(fc_lf.size(), rollout_number);
+    Tc_lh = Eigen::MatrixXd::Zero(fc_lf.size(), rollout_number);
+    Tc_rf = Eigen::MatrixXd::Zero(fc_lf.size(), rollout_number);
+    Tc_rh = Eigen::MatrixXd::Zero(fc_lf.size(), rollout_number);
+    Tau_c = Eigen::MatrixXd::Zero(nv, rollout_number);
+    Tau_c_raw = Eigen::MatrixXd::Zero(nv, rollout_number);
+
+    MomentumBasedFObs::Reg6D lambda = Eigen::VectorXd::Zero(6);
+    lambda << 0.000000000000001,
+              0.000000000000001,
+              0.000000000000001,
+              0.000000000000001,
+              0.000000000000001,
+              0.000000000000001;
+
+    std::vector<int> selector{0, 1, 2}; // only force
+
+    std::vector<std::string> contacts{"ball_1", "ball_2", "ball_3", "ball_4"};
+
+    bool regularize_w = true;
+
+    MomentumBasedFObs::Ptr f_obs_ptr(new MomentumBasedFObs(model_ptr, dt,
+                                                           contacts,
+                                                           BW,
+                                                           lambda, regularize_w,
+                                                           selector));
+
+    for (int i = 0; i < rollout_number; i++)
+    {
+        // we leave the state of the model fixed, so we are looking
+        // at the homogenous part of the observer dynamics.
+
+        f_obs_ptr->update();
+
+        f_obs_ptr->get_tau_obs(tau_c);
+        f_obs_ptr->get_f_est_at(contacts[0], fc_lf);
+        f_obs_ptr->get_f_est_at(contacts[1], fc_lh);
+        f_obs_ptr->get_f_est_at(contacts[2], fc_rf);
+        f_obs_ptr->get_f_est_at(contacts[3], fc_rh);
+        f_obs_ptr->get_t_est_at(contacts[0], tc_lf);
+        f_obs_ptr->get_t_est_at(contacts[1], tc_lh);
+        f_obs_ptr->get_t_est_at(contacts[2], tc_rf);
+        f_obs_ptr->get_t_est_at(contacts[3], tc_rh);
+
+        Fc_lf.block(0, i, Fc_lf.rows(), 1) = fc_lf;
+        Fc_lh.block(0, i, Fc_lf.rows(), 1) = fc_lh;
+        Fc_rf.block(0, i, Fc_lf.rows(), 1) = fc_rf;
+        Fc_rh.block(0, i, Fc_lf.rows(), 1) = fc_rh;
+        Tc_lf.block(0, i, Fc_lf.rows(), 1) = tc_lf;
+        Tc_lh.block(0, i, Fc_lf.rows(), 1) = tc_lh;
+        Tc_rf.block(0, i, Fc_lf.rows(), 1) = tc_rf;
+        Tc_rh.block(0, i, Fc_lf.rows(), 1) = tc_rh;
+
+        Tau_c.block(0, i, Tau_c.rows(), 1) = tau_c;
+        Tau_c_raw.block(0, i, Tau_c.rows(), 1) = tau_c_raw_static;
+
+    }
+
+    std::cout << "** QUADRUPED DEBUG PRINTS**\n"  << std::endl;
     std::cout << "\nURDF loaded at: "<< model_ptr->get_urdf_path() << "\n " << std::endl;
     std::cout << "** tau_c: \n" << Tau_c << "\n " << std::endl;
     std::cout << "** tau_c_raw: \n" << Tau_c_raw << "\n " << std::endl;
@@ -281,6 +398,7 @@ int main(int argc, char **argv) {
   urdf_path_fixed = urdf_fullpath_fixed;
   urdf_path_floating = urdf_fullpath_floating;
   urdf_anymal = URDF_PATH + std::string("anymal.urdf");
+  urdf_quadruped = URDF_PATH + std::string("quadruped.urdf");
 
   return RUN_ALL_TESTS();
 }
